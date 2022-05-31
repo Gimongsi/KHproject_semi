19960f4fe23e9dd3bc3d5565ba333b0bdebb29df
J <J>
2022-05-31 17:53:39 +0900

    information.jsp created

---
+++ src/main/java/com/dangpert/controller/InformationController.java
@@ -0,0 +1,103 @@
+package com.dangpert.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dangpert.dao.InformationDAO;
+import com.dangpert.dto.InfomationDTO;
+
+@WebServlet("/.info")
+public class InformationController extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
+	}
+
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		System.out.println("요청 uri : " + uri);
+		request.setCharacterEncoding("utf-8");
+		
+		
+		
+		if(uri.equals("/information/information.jsp")) { //게시판페이지 요청
+			InformationDAO dao = new InformationDAO();
+			try {
+				ArrayList<InfomationDTO> list = dao.selectAll(0, 0);
+				request.setAttribute("list", list);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}			
+			request.getRequestDispatcher("/manager/information/information.jsp").forward(request, response);
+		}else if(uri.equals("/write.info")) { // 글쓰기페이지 요청
+			response.sendRedirect("/information/write.jsp");
+		}else if(uri.equals("/writeProc.info")) { // 글쓰기 요청
+			InfomationDTO dto = (InfomationDTO)request.getSession().getAttribute("loginSession");
+			String qna_title = request.getParameter("qna_title");
+			String qna_content = request.getParameter("qna_content");
+
+			System.out.println(qna_title + " : " + qna_content);
+
+			InformationDAO dao = new InformationDAO();
+			try {
+				int rs = dao.write(new InfomationDTO(0,qna_title,qna_content));
+				if(rs > 0) {
+					response.sendRedirect("/information.info");
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}			
+			
+		}else if(uri.equals("/modify.info")) {// 수정 페이지 요청
+			int qna_seq = Integer.parseInt(request.getParameter("qna_seq"));
+			System.out.println("qna_seq : " + qna_seq);
+
+			InformationDAO dao = new InformationDAO();
+			try {
+				InfomationDTO dto = dao.selectBySeq(qna_seq);
+				request.setAttribute("dto", dto);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+
+			request.getRequestDispatcher("/manager/information/modify.jsp").forward(request, response);
+		}else if(uri.equals("/modifyProc.info")) { // 수정 요청
+			int qna_seq = Integer.parseInt(request.getParameter("qna_seq"));
+			String qna_title = request.getParameter("qna_title");
+			String qna_content = request.getParameter("qna_content");
+
+			InformationDAO dao = new InformationDAO();
+			try {
+				int rs = dao.modify(new InfomationDTO(0,qna_title,qna_content));
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}else if(uri.equals("/deleteProc.info")) {// 삭제 요청
+			int qna_seq = Integer.parseInt(request.getParameter("qna_seq"));
+			System.out.println("qna_seq :" + qna_seq);
+
+			InformationDAO dao = new InformationDAO();
+			
+			try {
+				int rs = dao.delete(qna_seq);
+				if(rs > 0) {
+					// 삭제완료 후에는 게시글 목록을 요청
+					response.sendRedirect("/information.info");
+				}
+			}catch(Exception e) {	
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/main/java/com/dangpert/dao/InformationDAO.java
@@ -0,0 +1,195 @@
+package com.dangpert.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+
+import com.dangpert.dto.InfomationDTO;
+
+public class InformationDAO {
+	private BasicDataSource bds;
+
+	public InformationDAO() {
+		try {
+			Context iCtx = new InitialContext();
+			Context envCtx = (Context)iCtx.lookup("java:comp/env");
+			bds = (BasicDataSource)envCtx.lookup("jdbc/bds");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int getNewSeq() throws Exception{
+		String sql = "select infomation_seq.nextval from dual";
+		try(Connection con = bds.getConnection();
+			PreparedStatement pstmt = con.prepareStatement(sql)){
+			
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			int qna_seq = rs.getInt(1);
+			return qna_seq;			
+		}
+	}
+
+	public ArrayList<InfomationDTO> searchByTitle(String searchKeyword) throws Exception{
+		String sql = "select * from tbl_infomation where title like '%'||?||'%' order by 1 desc";
+		try(Connection con = bds.getConnection();
+			PreparedStatement pstmt = con.prepareStatement(sql)){
+
+			pstmt.setString(1, searchKeyword);
+			
+			ResultSet rs = pstmt.executeQuery();
+			ArrayList<InfomationDTO> list = new ArrayList<>();
+			while(rs.next()) {
+				int qna_seq = rs.getInt("qna_seq");
+				String qna_title = rs.getString("qna_title");
+				String qna_content = rs.getString("qna_content");
+				list.add(new InfomationDTO(qna_seq,qna_title,qna_content));
+			}
+			return list;
+		}
+	}
+
+	public ArrayList<InfomationDTO> selectAll(int start, int end) throws Exception{
+		String sql = "select * from (select tbl_infomation.*, row_number() over(order by seq_board desc) as num from tbl_board)"
+					+ " where num between ? and ?";
+		try(Connection con = bds.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+			
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, end);
+
+			ResultSet rs = pstmt.executeQuery();
+			ArrayList<InfomationDTO> list = new ArrayList<>();
+			while(rs.next()) {
+				int qna_seq = rs.getInt("qna_seq");
+				String qna_title = rs.getString("qna_title");
+				String qna_content = rs.getString("qna_content");
+				list.add(new InfomationDTO(qna_seq,qna_title,qna_content));
+			}
+			return list;
+		}
+	}
+
+	public int delete(int qna_seq) throws Exception{
+		String sql = "delete from tbl_infomation where infomation_seq = ?";
+		try(Connection con = bds.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+
+			pstmt.setInt(1, qna_seq);
+			int rs = pstmt.executeUpdate();
+			return rs;
+		}
+	}
+
+	public int modify(InfomationDTO dto) throws Exception{
+		String sql = "update tbl_infomation set title=?, content=? where sinfomation_seq=?";
+		try(Connection con = bds.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+
+			pstmt.setInt(3, dto.getQna_seq());
+			pstmt.setString(1, dto.getQna_title());
+			pstmt.setString(2, dto.getQna_content());
+			int rs = pstmt.executeUpdate();
+			return rs;
+		}
+	}
+
+	public int write(InfomationDTO dto) throws Exception{
+		String sql = "insert into tbl_board values(?,?,?)";
+
+		try(Connection con = bds.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+
+			pstmt.setInt(3, dto.getQna_seq());
+			pstmt.setString(1, dto.getQna_title());
+			pstmt.setString(2, dto.getQna_content());
+
+			int rs = pstmt.executeUpdate();
+			return rs;
+		}
+	}
+
+	public InfomationDTO selectBySeq(int seq_board) throws Exception{
+		String sql = "select * from tbl_infomation where infomation_seq = ?";
+
+		try(Connection con = bds.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);){
+
+			pstmt.setInt(1, seq_board);
+			ResultSet rs = pstmt.executeQuery();
+
+			if(rs.next()) {
+				int qna_seq = rs.getInt("qna_seq");
+				String qna_title = rs.getString("qna_title");
+				String qna_content = rs.getString("qna_content");
+				InfomationDTO dto = new InfomationDTO(qna_seq, qna_title, qna_content);
+				return dto;
+			}
+			return null;
+		}
+	}
+
+	public HashMap<String, Object> getPageNavi(int curPage) throws Exception{
+		String sql = "select count(*) as totalCnt from tbl_infomation";
+		try(Connection con = bds.getConnection();
+			PreparedStatement pstmt = con.prepareStatement(sql)){
+			
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			
+			int totalCnt = rs.getInt("totalCnt"); 
+			int recordCntPerPage = 10; 
+			int naviCntPerPage = 5; 
+			int pageTotalCnt = 0; 
+		
+			if(totalCnt % recordCntPerPage > 0) { 
+				pageTotalCnt = totalCnt / recordCntPerPage + 1;
+			}else {
+				pageTotalCnt = totalCnt / recordCntPerPage;
+			}
+			
+			
+			if(curPage < 1) { 
+				curPage = 1;
+			}else if(curPage > pageTotalCnt) { 
+				curPage = pageTotalCnt; 
+			}					
+		
+			int startNavi = ((curPage-1) / naviCntPerPage) * naviCntPerPage + 1;
+			int endNavi = startNavi + naviCntPerPage - 1;
+			
+			
+			if(pageTotalCnt < endNavi) {
+				endNavi = pageTotalCnt;  
+			}
+			
+			boolean needPrev = true;
+			boolean needNext = true;
+			
+			if(startNavi == 1) {
+				needPrev = false;
+			}
+			if(endNavi == pageTotalCnt) {
+				needNext = false;
+			}
+		
+			HashMap<String, Object> map = new HashMap<>();
+			map.put("startNavi", startNavi);
+			map.put("endNavi", endNavi);
+			map.put("needPrev", needPrev);
+			map.put("needNext", needNext);
+			
+			return map;
+		}
+	}
+	
+}
+
